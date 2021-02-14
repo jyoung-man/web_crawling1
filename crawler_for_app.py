5c8a0343e0c515dbd3e86de9929fe7d9a0277ae8
jyoung-man <53083691+jyoung-man@users.noreply.github.com>
2021-02-14 11:51:19 +0900

    add professor table, merge lecture table

--- crawler_for_app.py
+++ crawler_for_app.py
@@ -13,8 +13,6 @@ import re
 import time
 from bs4 import BeautifulSoup  #refecence: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
-
-    
 def get_url_contents(url):
     s = None
     try:
@@ -60,103 +58,69 @@ def get_dept_code():
     con.commit()
     return code_num
 
-def make_major_db(code):
+def insert_into_db(url, c):
     con = sqlite3.connect('./iku.sqlite')
     cur = con.cursor()
-    cur.execute("DROP TABLE major;")
-    cur.execute("CREATE TABLE major(type text, l_number text, l_name text, credit integer, d_code text, time text, prof text, untact text, note text, section text);")
-    url = "https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2021&ltShtm=B01011"
-    for c in code:
-        dept = "&openSust="+c
-        sc, fc = get_url_contents(url+dept)
-        parse = BeautifulSoup(fc, 'html.parser')
-        datas = parse.find_all("tr", {"class": "table_bg_white"})
-        if len(datas) < 10:
-            continue
-        data = []
-        print(c)
-        time.sleep(2)
-        for t in datas[1:]:
-            data = t.text.split('\n')
-            k_lecture = data[5].split('(')
-            data[5] = k_lecture[0]
-            _prof = data[11].split(',')
-            data[11] = _prof[0]
-            cur = con.cursor()
-            cur.execute("INSERT INTO major VALUES(:type, :l_number, :l_name, :credit, :d_code, :time, :prof, :untact, :note, :section);", {"type":data[3], "l_number":data[4], "l_name":data[5], "credit":data[7], "d_code":c , "time":data[10], "prof":data[11], "untact":data[17], "note":data[22], "section":data[20]})
-            '''
-            print(data[3]) #이수구분
-            print(data[4]) #과목번호
-            print(data[5]) #강의명
-            print(data[7]) #학점
-            print(data[9]) #수강학과 #이거 말고 code 안의 과목번호를 넣자
-            print(data[10]) #시간, 장소
-            print(data[11]) #담당교수
-            '''
-            print(data[17]) #녹화:실시간:대면
-            print(data[22]) #note
+    sc, fc = get_url_contents(url+c)
+    parse = BeautifulSoup(fc, 'html.parser')
+    datas = parse.find_all("tr", {"class": "table_bg_white"})
+    if len(datas) < 10: 
+        return #NULL POINT EXCEPTION 방지. 정보가 안 나오는 강의는 조회 안하도록
+    data = []
+    print(c)
+    time.sleep(2) #트래픽 한 번에 너무 많이 몰리지 않게
+    for t in datas[1:]:
+        data = t.text.split('\n')
+        k_lecture = data[5].split('(')
+        data[5] = k_lecture[0]
+        when, where = calculate_time(data[10].strip())
+        _prof = data[11].split(',')
+        data[11] = _prof[0].strip()
+        cur = con.cursor()
+        cur.execute("INSERT INTO lecture VALUES(:type, :l_number, :l_name, :d_code, :prof, :section);", {"type":data[3], "l_number":data[4], "l_name":data[5], "d_code":c , "prof":data[11], "section":data[20]})
+        cur.execute("REPLACE INTO lec_info VALUES(:l_number, :credit, :time, :classroom, :untact, :note);", {"l_number":data[4], "credit":data[7], "time":when, "classroom":where, "untact":data[17], "note":data[22]})
+        '''
+        print(data[3]) #이수구분
+        print(data[4]) #과목번호
+        print(data[5]) #강의명
+        print(data[7]) #학점
+        print(data[9]) #수강학과 #이거 말고 code 안의 과목번호를 넣자
+        print(data[10]) #시간, 장소
+        print(data[11]) #담당교수
+        print(data[17]) #녹화:실시간:대면
+        print(data[22]) #note
+        '''
+    con.commit()
 
-        #print(name)
-        #담당 교수님이 두 분 이상인 강의 검색하는 법: where length(교수)>7(공백, 콤마 포함하여)
+def make_lecture_db(code):
+    con = sqlite3.connect('./iku.sqlite')
+    cur = con.cursor()
+    cur.execute("DROP TABLE lecture;")
+    cur.execute("CREATE TABLE lecture(type text, l_number text, l_name text, d_code text, prof text, section text);")
+    cur.execute("DROP TABLE lec_info;")
+    cur.execute("CREATE TABLE lec_info(l_number text, credit integer, time text, classroom text, untact text, note text);")
+    for c in code:
+        url = "https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2021&ltShtm=B01011&openSust="
+        insert_into_db(url, c)
         
-    con.commit()
-    print(cur.fetchall())
-    
-def make_curtural_db():
-    con = sqlite3.connect('./iku.sqlite')
-    cur = con.cursor()
-    cur.execute("DROP TABLE cultural;")
-    cur.execute("CREATE TABLE cultural(type text, l_number text, l_name text, credit integer, d_code text, time text, prof text, untact text, note text, section text);")
-    url = "https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2021&ltShtm=B01011&pobtDiv="
-    code = ["B0404P", "B04054"]
-    for c in code:
-        sc, fc = get_url_contents(url+c)
-        parse = BeautifulSoup(fc, 'html.parser')
-        datas = parse.find_all("tr", {"class": "table_bg_white"})
-        if len(datas) < 10:
-            continue
-        data = []
-        time.sleep(2)
-        for t in datas[1:]:
-            data = t.text.split('\n')
-            k_lecture = data[5].split('(')
-            data[5] = k_lecture[0]
-            _prof = data[11].split(',')
-            data[11] = _prof[0]
-            cur = con.cursor()
-            cur.execute("INSERT INTO cultural VALUES(:type, :l_number, :l_name, :credit, :d_code, :time, :prof, :untact, :note, :section);", {"type":data[3], "l_number":data[4], "l_name":data[5], "credit":data[7], "d_code":c , "time":data[10], "prof":data[11], "untact":data[17], "note":data[22], "section":data[20]})
-            '''
-            print(data[3]) #이수구분
-            print(data[4]) #과목번호
-            print(data[5]) #강의명
-            print(data[7]) #학점
-            print(data[9]) #수강학과 #이거 말고 code 안의 과목번호를 넣자
-            print(data[10]) #시간, 장소
-            print(data[11]) #담당교수
-            '''
-            print(data[17]) #녹화:실시간:대면
-            print(data[22]) #note
-            print(data[20]) #section, 영역
-
-        #print(name)
-        #담당 교수님이 두 분 이상인 강의 검색하는 법: where length(교수)>7(공백, 콤마 포함하여)
+    type_code = ["B0404P", "B04054"]
+    for t in type_code:
+        url = "https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2021&ltShtm=B01011&pobtDiv="
+        insert_into_db(url, t)
     con.commit()
     
-def dept_for_me():
-    
+def show_dept_for_me():
     url = 'https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2021&ltShtm=B01011'
     sc, fc = get_url_contents(url)
     parse = BeautifulSoup(fc, 'html.parser')
     depts = parse.find_all("select", {"name": "openSust"}) 
     dept = []
     code = []
-
     for t in depts:
         name = t
         print(name)
         code.append(str(t))
         dept.append(name)
-    
     return
 
 def delete_more_dept():
@@ -167,37 +131,7 @@ def delete_more_dept():
     for d in delete:
         cur = con.cursor()
         cur.execute('DELETE FROM dept WHERE d_code = "{}";'.format(d))
-        cur.execute('DELETE FROM major WHERE d_code = "{}";'.format(d))
-    con.commit()
-    
-        
-def insert_lecture(dept):
-    con = sqlite3.connect('./iku.sqlite')
-    cur = con.cursor()
-    url = "https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2021&ltShtm=B01011&openSust="
-    sc, fc = get_url_contents(url+dept)
-    parse = BeautifulSoup(fc, 'html.parser')
-    datas = parse.find_all("tr", {"class": "table_bg_white"})
-    data = []
-    print(dept)
-    time.sleep(2)
-    for t in datas[1:]:
-        data = t.text.split('\n')
-        k_lecture = data[5].split('(')
-        data[5] = k_lecture[0]
-        _prof = data[11].split(',')
-        data[11] = _prof[0]
-        cur = con.cursor()
-        cur.execute("INSERT INTO major VALUES(:type, :l_number, :l_name, :credit, :d_code, :time, :prof, :untact, :note);", {"type":data[3], "l_number":data[4], "l_name":data[5], "credit":data[7], "d_code":dept , "time":data[10], "prof":data[11], "untact":data[17], "note":data[22], "section":data[20]})
-        '''
-        print(data[3]) #이수구분
-        print(data[4]) #과목번호
-        print(data[5]) #강의명
-        print(data[7]) #학점
-        print(data[9]) #수강학과 #이거 말고 code 안의 과목번호를 넣자
-        print(data[10]) #시간, 장소
-        print(data[11]) #담당교수
-        '''
+        #cur.execute('DELETE FROM lecture WHERE d_code = "{}";'.format(d))
     con.commit()
     
 def update_lecture_data():
@@ -210,39 +144,204 @@ def update_lecture_data():
         del items[0]
         for i in items:
             print(i)
-            sql = 'update major set d_code = "{}" where l_number = "{}";'.format(dept, i)
+            sql = 'update lecture set d_code = "{}" where l_number = "{}";'.format(dept, i)
             cur = con.cursor()
             cur.execute(sql)
             print(sql)
         line = f.readline()
         if not line: break
     college = ["105271", "126897", "102761", "104951", "127119", "122045", "105121", "105061"]
-    cur.execute('update major set d_code = "105091" where d_code = "105061";')
+    cur.execute('update lecture set d_code = "105091" where d_code = "105061";')
     cur.execute('update dept set d_name = "건축대학 건축학부" where d_code = "121135";')
     for c in college:
         cur.execute('DELETE FROM dept WHERE d_code = {};'.format(c))
     con.commit()
     f.close()
 
+def get_prof_database():
+    con = sqlite3.connect('./iku.sqlite')
+    cur = con.cursor()
+    cur.execute("DROP TABLE professor;")
+    cur.execute("CREATE TABLE professor(prof text, lab text, contact text);")
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_02_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_02_02_tab01.jsp"    
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_01_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_02_01_05_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_03_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_01_03_04_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_02_01_01_tab01.jsp"
+    insert_prof_data(url)
+    #url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_02_01_02_tab01.jsp"
+    #insert_prof_data(url)
+    #물리학과
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_02_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_03_01_04_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_11_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_04_10_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_04_03_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_04_04_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_05_01_04_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_04_05_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_15_01_05_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_04_04_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_04_08_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_06_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_08_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_06_01_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_08_01_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_08_02_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_15_01_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_15_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_09_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_09_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_04_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_05_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_06_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_07_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_10_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_08_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_20_01_09_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_12_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_13_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_13_01_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_13_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_13_01_05_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_13_02_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_13_02_05_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_01_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_02_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_03_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_04_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_05_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_06_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_14_01_07_tab01.jsp"
+    insert_prof_data(url)
+    url = "https://www.konkuk.ac.kr/jsp/Coll/coll_01_19.jsp"
+    insert_prof_data(url)
+    #부동산학과, 물리학과, 생과대 안넣었음    
+    
+def insert_prof_data(url):
+    con = sqlite3.connect('./iku.sqlite')
+    cur = con.cursor()
+    sc, fc = get_url_contents(url)
+    parse = BeautifulSoup(fc, 'html.parser')
+    datas = parse.find_all("tr")
+    data = []
+    for d in datas[1:]:
+        data = d.text.split("\n")
+        cur.execute('INSERT INTO professor VALUES(:prof, :lab, :contact);', {"prof":data[2], "lab":data[5], "contact":data[7]})
+        print(data[2]) #교수님 성함
+        print(data[5]) #연구실
+        print(data[7]) #이메일       
+    con.commit()
+
+def calculate_time(info):
+    info = info.replace(" ", "")
+    days = info.split(",")
+    when = ""
+    where = ""
+    for i in range(len(days)):
+        if i>0:
+            when += ','
+            where += ','
+        time = []        
+        temp = re.findall("\d+", days[i])
+
+        mark = days[i].find('(')
+        where += days[i][mark:]
+
+        for t in temp:
+            k = float(t)/2 + 9
+            sc = str(k)
+            val = str(sc).find(".5")
+            if val == -1:
+                sc = sc.replace(".0", ":00")
+            else:
+                sc = sc.replace(".5", ":30")
+            time.append(sc)
+        #print(time)
+        if len(time) > 1:    
+            when += (days[i][0] + time[0] + "-" + time[1])
+        elif len(time) > 0:
+            when += days[i][0]
+        else:
+            continue
+    #print(when)
+    return when, where
+    
     
 def refresh_db():
     code = get_dept_code()
-    make_major_db(code)
-    make_curtural_db() 
+    make_lecture_db(code)
     update_lecture_data()
-
+    get_prof_database()
+    
 if __name__ == '__main__':
     #dept_for_me()
     #dept = "105271"
     con = sqlite3.connect('./iku.sqlite')
     cur = con.cursor()
-    cur.execute('update dept set d_name = "건축대학 건축학부" where d_code = "121135";')
-    con.commit()
-    cur.execute('SELECT * FROM dept;')
-    #cur.execute('select * from major where d_code = "105061";');
-    #cur.execute('SELECT * FROM cultural;')
-    #print(cur.fetchall())
-
-    for i in range(100):
+    cur.execute('select * from lecture where prof = "SANGILIYANDI";')
+    print(cur.fetchall())
+    '''
+    cur.execute('select * from professor;')
+    for i in range(600):
         print(cur.fetchone())
+    '''
+    #cur.execute('update dept set d_name = "건축대학 건축학부" where d_code = "121135";')
+    #con.commit()
+    #refresh_db()
+    #get_prof_database()
 
